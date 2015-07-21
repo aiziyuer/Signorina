02306489dd4e1bcb2f5f451d626597f266f385a0
lc <ziyu0123456789@gmail.com>
2015-07-21 23:59:44 +0800

    工程初步建立

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 应用入口文件
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    }
+
+}
