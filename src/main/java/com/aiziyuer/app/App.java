facb7ecf51b18c03df492a214b86fc81f27ae75b
lc <ziyu0123456789@gmail.com>
2015-07-22 01:48:42 +0800

    针对Eclipse进行配置调整

--- src/main/java/com/aiziyuer/app/App.java
+++ src/main/java/com/aiziyuer/app/App.java
@@ -2,7 +2,9 @@ package com.aiziyuer.app;
 
 import com.aiziyuer.app.fiberLink.IFiberService;
 import com.aiziyuer.app.framework.util.ServiceLocator;
+
 import lombok.extern.log4j.Log4j2;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
@@ -13,11 +15,14 @@ import java.io.IOException;
 @Log4j2
 public class App {
 
-    public static void main(String[] args) throws IOException {
+    @SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
 
         log.info("spring init start.");
         long currentTime = System.currentTimeMillis();
+        
         new ClassPathXmlApplicationContext("applicationContext.xml");
+        
         log.info(String.format("spring init end, cost(%ss).", System.currentTimeMillis() - currentTime));
 
         IFiberService fiberService = ServiceLocator.getInstance().getService("fiberService");
